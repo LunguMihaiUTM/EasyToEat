10def13d7b5c65d031a6945923d9e0723bea8b70
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-03-21 23:46:39 +0200

    Small adjustment for the new added design patterns : Adapter, Composite, Facade

---
+++ src/main/java/agin/llc1/llc1/repository/TableRepository.java
@@ -0,0 +1,7 @@
+package agin.llc1.llc1.repository;
+
+import agin.llc1.llc1.entity.Table;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableRepository extends JpaRepository<Table, Long> {
+}
